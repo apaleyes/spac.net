80149a746d9a4c66d4bf1a3908b60abd7737c65a
Andrei Paleyes <apalees@gmail.com>
2015-03-27 01:13:40 +0000

    Sequential prefix sum example from chaprter 5

---
+++ sources/spac.net/spac.net.tests/Chapter5/SequentialPrefixSumTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using spac.net.Chapter5;
+
+namespace spac.net.tests.Chapter5
+{
+    class SequentialPrefixSumTests
+    {
+        [Test]
+        public void should_calculate_prefix_sums()
+        {
+            int[] arr = Enumerable.Range(0, 1000).ToArray();
+            int[] expected_output = Enumerable.Range(0, 1000)
+                                              .Select(i => i * (i + 1) / 2)
+                                              .ToArray();
+
+            int[] output = SequentialPrefixSum.PrefixSum(arr);
+
+            Assert.AreEqual(expected_output, output);
+        }
+    }
+}
---
+++ sources/spac.net/spac.net/Chapter5/SequentialPrefixSum.cs
@@ -0,0 +1,17 @@
+﻿namespace spac.net.Chapter5
+{
+    public class SequentialPrefixSum
+    {
+        public static int[] PrefixSum(int[] input)
+        {
+            int[] output = new int[input.Length];
+            output[0] = input[0];
+            for (int i = 1; i < input.Length; i++)
+            {
+                output[i] = output[i-1] + input[i];
+            }
+
+            return output;
+        }
+    }
+}
