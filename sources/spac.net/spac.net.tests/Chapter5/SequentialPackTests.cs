76c098fedc0511fa5477f027a0a5fb31267cc74d
Andrei Paleyes <apalees@gmail.com>
2015-03-28 00:38:23 +0000

    Sequential pack implementation

---
+++ sources/spac.net/spac.net.tests/Chapter5/SequentialPackTests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using spac.net.Chapter5;
+
+namespace spac.net.tests.Chapter5
+{
+    class SequentialPackTests
+    {
+        [Test]
+        public void should_output_correct_pack_array()
+        {
+            int[] input = new Randomizer().GetInts(0, 100, 100);
+            int[] expected_output = input.Where(i => i > 10).ToArray();
+
+            int[] output = SequentialPack.GreaterThenTen(input);
+
+            Assert.AreEqual(expected_output, output);
+        }
+    }
+}
---
+++ sources/spac.net/spac.net/Chapter5/SequentialPack.cs
@@ -0,0 +1,28 @@
+﻿namespace spac.net.Chapter5
+{
+    public class SequentialPack
+    {
+        public static int[] GreaterThenTen(int[] input)
+        {
+            int count = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] > 10)
+                    count++;
+            }
+
+            int[] output = new int[count];
+            int index = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] > 10)
+                {
+                    output[index] = input[i];
+                    index++;
+                }
+            }
+
+            return output;
+        }
+    }
+}
