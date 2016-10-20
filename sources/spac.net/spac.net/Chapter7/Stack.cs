3fdb0b04e9faa9c12ae127a557199963cca8a42f
Andrei Paleyes <apalees@gmail.com>
2016-10-21 00:53:15 +0100

    Added Stack code for first part of Chapter 7

---
+++ sources/spac.net/spac.net/Chapter7/Stack.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spac.net.Chapter7
+{
+    class Stack<E>
+    {
+        private E[] array;
+        private int index = 0;
+
+        public Stack(int size)
+        {
+            array = new E[size];
+        }
+
+        public bool IsEmpty()
+        {
+            lock (this)
+            {
+                return index == 0;
+            }
+        }
+
+        public void Push(E val)
+        {
+            lock (this)
+            {
+                if (index == array.Length)
+                {
+                    throw new StackFullException();
+                }
+                array[index++] = val;
+            }
+        }
+
+        public E Pop()
+        {
+            lock (this)
+            {
+                if (index == 0)
+                {
+                    throw new StackEmptyException();
+                }
+                return array[--index];
+            }
+        }
+
+        public E Peek1()
+        {
+            lock (this)
+            {
+                if (index == 0)
+                {
+                    throw new StackEmptyException();
+                }
+                return array[index - 1];
+            }
+        }
+
+        public E Peek2()
+        {
+            lock (this)
+            {
+                E ans = Pop();
+                Push(ans);
+                return ans;
+            }
+        }
+    }
+
+    class StackFullException : Exception
+    { }
+
+    class StackEmptyException :  Exception
+    { }
+}
---
+++ sources/spac.net/spac.net/Chapter7/StackHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spac.net.Chapter7
+{
+    class CorrectStackHelper
+    {
+        static E PeekHelper<E>(Stack<E> s)
+        {
+            lock (s)
+            {
+                E ans = s.Pop();
+                s.Push(ans);
+                return ans;
+            }
+        }
+    }
+
+    class WrongStackHelper
+    {
+        static E PeekHelper<E>(Stack<E> s)
+        {
+            E ans = s.Pop();
+            s.Push(ans);
+            return ans;
+        }
+    }
+}
