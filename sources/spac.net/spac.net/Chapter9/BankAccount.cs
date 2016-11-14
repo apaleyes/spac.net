340044f8e1d8b7a006a54924067110fef41348a0
Andrei Paleyes <apalees@gmail.com>
2016-11-14 00:29:08 +0000

    Implemented example for deadlock chapter

---
+++ sources/spac.net/spac.net/Chapter9/BankAccount.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace spac.net.Chapter9
+{
+    class BankAccount
+    {
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        void Withdraw(int amt) { }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        void Deposit(int amt) { }
+
+        [MethodImpl(MethodImplOptions.Synchronized)]
+        void TransferToWrong(int amt, BankAccount a)
+        {
+            this.Withdraw(amt);
+            a.Deposit(amt);
+        }
+
+        void TransferToUnsync(int amt, BankAccount a)
+        {
+            this.Withdraw(amt);
+            a.Deposit(amt);
+        }
+        
+        void TransferToAtomicWrong(int amt, BankAccount a)
+        {
+            lock (this)
+            {
+                lock (a)
+                {
+                    this.Withdraw(amt);
+                    a.Deposit(amt);
+                }
+            }
+        }
+
+        private int acctNumber;
+        void transfertToAtomic(int amt, BankAccount a)
+        {
+            if (this.acctNumber < a.acctNumber)
+            {
+                lock (this)
+                {
+                    lock (a)
+                    {
+                        this.Withdraw(amt);
+                        a.Deposit(amt);
+                    }
+                }
+            }
+            else
+            {
+                lock (a)
+                {
+                    lock (this)
+                    {
+                        this.Withdraw(amt);
+                        a.Deposit(amt);
+                    }
+                }
+            }
+        }
+    }
+}
