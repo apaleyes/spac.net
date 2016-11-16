2cfa6910ebccf024780013b43a6728fa4c6c6ef8
Andrei Paleyes <apalees@gmail.com>
2016-11-16 00:40:55 +0000

    Added Buffer classes for conditional variables illustration

---
+++ sources/spac.net/spac.net/Chapter10/BufferLocksOnly.cs
@@ -0,0 +1,41 @@
+﻿namespace spac.net.Chapter10
+{
+    class BufferLocksOnly<E>
+    {
+        bool IsEmpty() { return true; }
+        bool IsFull() { return true; }
+
+        // not shown: an array of fixed size for the queue with two indices
+        // for the front and back, along with methods IsEmpty() and IsFull()
+        void Enqueue(E elt)
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    if (!IsFull())
+                    {
+                        // do enqueue
+                        return;
+                    }
+                }
+            }
+        }
+
+        E Dequeue()
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    if (!IsEmpty())
+                    {
+                        E result = default(E);
+                        // do dequeue
+                        return result;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ sources/spac.net/spac.net/Chapter10/BufferWrongNotifyWait.cs
@@ -0,0 +1,48 @@
+﻿using System.Threading;
+
+namespace spac.net.Chapter10
+{
+    class BufferWrongNotifyWait<E>
+    {
+        bool IsEmpty() { return true; }
+        bool IsFull() { return true; }
+
+        // not shown: an array of fixed size for the queue with two indices
+        // for the front and back, along with methods IsEmpty() and IsFull()
+        void Enqueue(E elt)
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    if (IsFull())
+                    {
+                        Monitor.Wait(this);
+                    }
+                    // do enqueue as normal
+                    //Uncomment this: if (...buffer was empty (i.e., now has 1 element) ...)
+                    Monitor.Pulse(this);
+                }
+            }
+        }
+
+        E Dequeue()
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    if (IsEmpty())
+                    {
+                        Monitor.Wait(this);
+                    }
+                    E ans = default(E);
+                    // E ans = do dequeue as normal
+                    //Uncomment this: if(... buffer was full (i.e., now has room for 1 element) ...)
+                    Monitor.Pulse(this);
+                    return ans;
+                }
+            }
+        }
+    }
+}
