b2aed99582bb7bba6e38086066b3a36726c23ead
Andrei Paleyes <apalees@gmail.com>
2017-02-13 07:47:35 +0000

    Added missing code sample for variable number of threads

---
+++ sources/spac.net/spac.net.tests/Chapter3/VariableNumberOfThreadsArraySumTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using spac.net.Chapter3;
+
+namespace spac.net.tests.Chapter3
+{
+    class VariableNumberOfThreadsArraySumTests
+    {
+        [TestFixture]
+        class CorrectThreadedArraySumTests
+        {
+            [Test]
+            public void should_calculate_array_sum()
+            {
+                int[] arr = Enumerable.Range(0, 1000).ToArray();
+
+                int sum = VariableNumberOfThreadsArraySum.Sum(arr, 3);
+
+                Assert.AreEqual(arr.Sum(), sum);
+            }
+        }
+    }
+}
---
+++ sources/spac.net/spac.net/Chapter3/VariableNumberOfThreadsArraySum.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading;
+
+namespace spac.net.Chapter3
+{
+    public class VariableNumberOfThreadsArraySum
+    {
+        class SumRange
+        {
+            int left;
+            int right;
+            int[] arr;
+            public int Answer { get; private set; }
+
+            public SumRange(int[] a, int left, int right)
+            {
+                this.left = left;
+                this.right = right;
+                this.arr = a;
+                Answer = 0;
+            }
+
+            public void Run()
+            {
+                for (int i = left; i < right; i++)
+                {
+                    Answer += arr[i];
+                }
+            }
+        }
+
+        public static int Sum(int[] arr, int numThreads)
+        {
+            int len = arr.Length;
+            int ans = 0;
+
+            SumRange[] s = new SumRange[numThreads];
+            Thread[] t = new Thread[numThreads];
+            for (int i = 0; i < numThreads; i++)
+            {
+                SumRange sr = new SumRange(arr, (i * len) / numThreads, ((i + 1) * len) / numThreads);
+                s[i] = sr;
+                t[i] = new Thread(sr.Run);
+                t[i].Start();
+            }
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                t[i].Join();
+                ans += s[i].Answer;
+            }
+
+            return ans;
+        }
+    }
+}
